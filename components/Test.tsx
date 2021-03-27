ecb590219ced84b0abae87747109b12b84cc9fc2
Weipeng jiang <weipengjiang@Weipengs-MacBook-Pro.local>
2021-03-27 20:14:25 +1300

    test

---
+++ components/Test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const Test = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default Test;
